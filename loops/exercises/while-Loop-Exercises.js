f25d28921439965342e57ce8b0219bdb7d82c6e4
celiapaivab <celiadepaivabruno@gmail.com>
2024-01-21 17:39:24 -0600

    Exercises Loop For done

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,24 +1,28 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
+let fuelLevel = 0;
 
+let numberAstr = 0;
 
-
+let altitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+const input = require('readline-sync');
 
+fuelLevel = input.question("Please enter the starting fuel level:");
+fuelLevel = Number(fuelLevel);
 
-
+while (fuelLevel <= 0) {
+  fuelLevel = input.question("Invalid input. Please enter a positive number:");
+  fuelLevel = Number(fuelLevel);
+}
+console.log (fuelLevel);
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
-  
-  
-  
+
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
