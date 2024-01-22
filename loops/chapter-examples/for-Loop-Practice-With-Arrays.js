4fdebce678e375d45abc33d0e378cf97365490a3
celiapaivab <celiadepaivabruno@gmail.com>
2024-01-22 17:05:26 -0600

    loops examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let family = ["Marilda", "Celia", "Rafaela", "Rafael"];
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < family.length; i++) {
+   console.log(family[i]);
+}
