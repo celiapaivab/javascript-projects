5eaa5d760e08cbd7e7812df114071796f1972d3c
celiapaivab <celiadepaivabruno@gmail.com>
2024-01-27 21:41:20 -0600

    Exercises More on Functions complete

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,28 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
+let isWordA = function(word) {
+
+  if (word.slice(0, 1) == "a") {
+  return true;
+  }
+
+  return false;
+};
+
+console.log(getValidInput('Enter a word with initial "a": ', isWordA));
 
 // TODO 2: write a validator 
 // that ensures input is a vowel
+let isVowel = function(letter) {
+
+  if (letter != "a" && letter != "e" && letter != "i" && letter != "o" && letter != "u") {
+  return false;
+  }
+
+  return true;
+};
+
+console.log(getValidInput('Enter a vowel: ', isVowel));
 
 // Be sure to test your code!
--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,6 +6,17 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+let practiceAnonymous = function(entry){
+    if(typeof entry === "number"){
+        return 3*entry;
+    } else if (typeof entry === "string") {
+        return "ARRR!";
+    } else {
+        return entry;
+    }
+}
+console.log(practiceAnonymous(false));
+
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +24,8 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+
+let changed = arr.map(practiceAnonymous);
+
+console.log(changed);
