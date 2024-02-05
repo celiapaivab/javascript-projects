3195b1c531cd543eaaca5142c06164e3d9d33cc5
celiapaivab <celiadepaivabruno@gmail.com>
2024-02-05 16:59:44 -0600

    Classes examples done

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,15 +3,33 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name, age, mass = 54){
      this.name = name;
      this.age = age;
      this.mass = mass;
    }
 }
 
-let tortoise = new Astronaut('Speedy', 120);
+let tortoise = new Astronaut('Speedy', 120, 50);
 
 console.log(tortoise.name, tortoise.age, tortoise.mass);
 
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+
+class Car {
+  constructor(make, model, year, color, mpg){
+     this.make = make;
+     this.model = model;
+     this.year = year;
+     this.color = color;
+     this.mpg = mpg;
+  }
+}
+
+// let myCar = new Car('Chevy', 'Astro', 1985, 'gray', 20);
+
+// console.log(typeof myCar.year);
+
+let myCar = new Car('Tesla', 'Model S', 2019);
+
+console.log(myCar);
